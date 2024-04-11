8942d027849484d8bf2d22d33131ad436d54936b
FreddyJR1995 <freddy.valverde@epn.edu.ec>
2024-04-10 23:24:40 -0500

    feat: [ece-7] add cart section and fix result table styles

--- src/assets/CartLogo/index.jsx
+++ src/assets/CartLogo/index.jsx
@@ -1,8 +1,15 @@
 import './CartLogo.css'
+import { useContext } from 'react';
+import { SearchContext } from '../../contexts/SearchContext';
 
 function CartLogo () {
+    const { setOpenCartSection, openCartSection } = useContext(SearchContext);
+
+    const handleCartSection =()=>{
+        setOpenCartSection(!openCartSection)
+    }
     return (
-        <div className='cartLogoContainer'>
+        <div className='cartLogoContainer' onClick={handleCartSection}>
             <img src='src/assets/CartLogo/shopping-cart.png'/>
         </div>
     )
